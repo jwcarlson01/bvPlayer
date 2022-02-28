1530b20e19fcb7d51f264470f215315ec55bdeaa
jwcarlson01 <62767291+jwcarlson01@users.noreply.github.com>
2022-02-27 21:04:53 -0500

    Update bvPlayer.py

--- bvPlayer/bvPlayer.py
+++ bvPlayer/bvPlayer.py
@@ -4,7 +4,7 @@ from . import VideoPlayer
 class bvPlayer:
     def __init__(self, file, **kwargs):
         root = Tk()
-        player = VideoPlayer(root, file, **kwargs)
+        player = VideoPlayer.VideoPlayer(root, file, **kwargs)
 
         try:
             player.play()
