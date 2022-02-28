86080da08ba3971fa7ced820d05edb099e00c6a2
jwcarlson01 <62767291+jwcarlson01@users.noreply.github.com>
2022-02-27 20:44:24 -0500

    TempDeletion
    Generated temp files are now stored in a temp folder per thread, these folders are deleted on exit. Cleanup on keyboard interrupt and cleanup on quit in options.

--- bvPlayer/bvPlayer.py
+++ bvPlayer/bvPlayer.py
@@ -4,5 +4,14 @@ from . import VideoPlayer
 class bvPlayer:
     def __init__(self, file, **kwargs):
         root = Tk()
-        player = VideoPlayer.VideoPlayer(root, file, **kwargs)
+        player = VideoPlayer(root, file, **kwargs)
+
+        try:
+            player.play()
+        except KeyboardInterrupt:
+            player.kill()
+            
         root.mainloop()
+
+bvPlayer('tree480.mp4', fps = 28, pos = (0, 0), draggable = True,
+         dim = (1260,720), videoOptions = True)
